fb4160ec2a8650a1ed8dc7a59bf5a621b0de55c2
Achyut <achyutp10@gmail.com>
2025-01-26 17:30:05 +0545

    db setup and bootstrap and listing data

---
+++ Bulky/BulkyWeb1/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb1.Data;
+using BulkyWeb1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb1.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCList = _db.Categories.ToList();
+            return View(objCList);
+        }
+    }
+}

bba8bbe674e70af2913ad1c3f763e67e9f9be0d1
Achyut <achyutp10@gmail.com>
2025-02-01 09:42:33 +0545

    Create Product with image in upsert

--- Bulky/BulkyWeb1/Areas/Admin/Controllers/ProductController.cs
+++ Bulky/BulkyWeb1/Areas/Admin/Controllers/ProductController.cs
@@ -12,9 +12,11 @@ namespace BulkyWeb1.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -61,6 +63,18 @@ namespace BulkyWeb1.Areas.Admin.Controllers
             }
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file!=null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using ( var fileStream = new FileStream(Path.Combine(productPath, fileName),FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    obj.Product.ImageUrl = @"\images\product\" + fileName;
+                }
                 _unitOfWork.Product.Add(obj.Product);
                 TempData["SuccessMessage"] = "Data Created";
 

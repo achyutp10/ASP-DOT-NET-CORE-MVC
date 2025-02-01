7ff583c3180ade2dacdae4f87cb6912e4b4be8c6
Achyut <achyutp10@gmail.com>
2025-02-01 21:51:46 +0545

    identity update migration and database

--- Bulky/BulkyWeb1/Program.cs
+++ Bulky/BulkyWeb1/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options=>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
@@ -32,7 +33,7 @@ app.UseRouting();
 app.UseAuthentication();    
 
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");

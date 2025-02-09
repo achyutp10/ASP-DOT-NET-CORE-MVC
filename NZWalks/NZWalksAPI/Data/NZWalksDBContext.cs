740fa042e9c85703d81d2705d93d518f5f314e6f
Achyut <achyutp10@gmail.com>
2025-02-09 19:57:51 +0545

    DB Context

---
+++ NZWalks/NZWalksAPI/Data/NZWalksDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Data
+{
+    public class NZWalksDBContext : DbContext
+    {
+        public NZWalksDBContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+        
+    }
+}

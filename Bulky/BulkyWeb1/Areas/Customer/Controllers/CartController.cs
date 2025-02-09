86cefc79551325dd7b15e71cac8c7678f6f70281
Achyut <achyutp10@gmail.com>
2025-02-09 13:57:18 +0545

    WEB API

--- Bulky/BulkyWeb1/Areas/Customer/Controllers/CartController.cs
+++ Bulky/BulkyWeb1/Areas/Customer/Controllers/CartController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb1.Areas.Customer.Controllers
 {
     [Area("customer")]
+    [Authorize]
     public class CartController : Controller
     {
         public IActionResult Index()

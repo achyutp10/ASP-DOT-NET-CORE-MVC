fced0958cc4476c45400cabaefa53e9e31d76474
Achyut <achyutp10@gmail.com>
2025-02-11 09:36:23 +0545

    create walks

---
+++ NZWalks/NZWalksAPI/Repositories/SQLWalkRepository.cs
@@ -0,0 +1,21 @@
+﻿using NZWalksAPI.Data;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Repositories
+{
+    public class SQLWalkRepository : IWalkRepository
+    {
+        private readonly NZWalksDBContext dBContext;
+
+        public SQLWalkRepository(NZWalksDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+        public async Task<Walk> CreateAsync(Walk walk)
+        {
+            await dBContext.Walks.AddAsync(walk);
+            await dBContext.SaveChangesAsync();
+            return walk;
+        }
+    }
+}

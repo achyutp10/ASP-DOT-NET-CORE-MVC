2655b1e13e3b9b6c338bdc86cb764ccbc3773cf6
Achyut <achyutp10@gmail.com>
2025-02-16 19:49:47 +0545

    validations for image upload in controller and making dto

---
+++ NZWalks/NZWalksAPI/Controllers/ImagesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Models.DTO;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        //POST: /api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request)
+        {
+            ValidateFileUpload(request);
+            if (ModelState.IsValid)
+            {
+                // Use Repository to upload image
+
+            }
+            return BadRequest(ModelState);
+        }
+        private void ValidateFileUpload(ImageUploadRequestDto request)
+        {
+            var allowedExtensions = new string[] { ".jpg", "jpeg", ".png" };
+            if (!allowedExtensions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsupported file extension");
+            }
+            if (request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File size more than 10 mb");
+
+            }
+        }
+
+
+    }
+}
